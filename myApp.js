cf2a40f52b22758fba4ff3753154c15cb7fea43e
eddieabasi <edbeeotudor@gmail.com>
2023-06-19 22:36:36 +0000

    chain middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
 app.use("/public", express.static(process.cwd() + "/public"));
+// Middleware functions are fncs that take 3 args:
+// the request object, the response object, and the next function in the app's request-response cycle.
 
+// simple logger
+app.use((req, res, next) => {
+  const output = `${req.method + ' ' + req.path + ' - ' + req.ip}`
+  console.log(output)
+  next()
+})
 // console.log("Hello World");
 
 // app.get('/', (req, res) => {
@@ -15,9 +23,20 @@ app.get("/", (req, res) => {
 });
 
 // send json response
-app.get('/json', (req, res) => {
-    let msg = "Hello json"
-     res.json({message: process.env.MESSAGE_STYLE === "uppercase" ? msg.toUpperCase() : msg});
+app.get("/json", (req, res) => {
+  let msg = "Hello json";
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? msg.toUpperCase() : msg,
+  });
 });
+
+// chain a middleware function
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({time: req.time});
+})
 
 module.exports = app;
